7bc14aaac63af5f78b2cead29aa03ca20a92b0a1
mustofa-k <kamalmustofa241@gmail.com>
2024-10-20 20:29:37 +0700

    perbaikan 4

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-// Tentukan API key dan base URL untuk TMDb
+// Tentukan API key dan base URL untuk TMDb dari environment variables
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
+const REDIRECT_URL = import.meta.env.VITE_REDIRECT_URL || "http://localhost:5173/callback"; // Redirect URL
 
 interface RequestTokenResponse {
   request_token: string;
@@ -18,7 +19,7 @@ interface AccountDetails {
   username: string;
 }
 
-// Fungsi untuk mendapatkan request token baru dari TMDb
+// Fungsi untuk mendapatkan request token dari TMDb
 export const getRequestToken = async (): Promise<string | null> => {
   try {
     const response = await axios.get<RequestTokenResponse>(`${API_BASE_URL}/authentication/token/new`, {
@@ -33,22 +34,14 @@ export const getRequestToken = async (): Promise<string | null> => {
 
 // Fungsi untuk mengarahkan pengguna ke halaman otorisasi TMDb
 export const redirectToAuthorization = (requestToken: string): void => {
-  const authUrl = `https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=https://testenterkode.vercel.app/callback`;
+  const authUrl = `https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${REDIRECT_URL}`;
   window.location.href = authUrl;
 };
 
 // Fungsi untuk membuat session ID setelah otorisasi
 export const createSession = async (requestToken: string): Promise<string | null> => {
   try {
-    const response = await axios.post<SessionResponse>(
-      `${API_BASE_URL}/authentication/session/new`,
-      {
-        request_token: requestToken,
-      },
-      {
-        params: { api_key: API_KEY },
-      }
-    );
+    const response = await axios.post<SessionResponse>(`${API_BASE_URL}/authentication/session/new`, { request_token: requestToken }, { params: { api_key: API_KEY } });
     return response.data.session_id;
   } catch (error) {
     if (axios.isAxiosError(error)) {
